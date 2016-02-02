6a34075cdb62ef2e558cd9fca4e466c7fc0b644a
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2016-02-02 10:55:30 +0200

    new branch

--- app/scripts/services/ajaxFactory.js
+++ app/scripts/services/ajaxFactory.js
@@ -1,14 +1,70 @@
 'use strict';
 
 angular
-  .module('chatApp')
+    .module('chatApp')
     .factory('AjaxFactory', function ($http) {
         var urlBase = 'http://users.metropolia.fi/~ilkkamtk/chatApi';
         var ajaxFunctions = {};
 
         ajaxFunctions.login = function (args) {
-            return $http.post(urlBase+ '/login', args);
+            return $http.post(urlBase + '/login', args);
+        };
+
+        ajaxFunctions.deleteMessage = function (args) {
+            return $http.delete(urlBase + '/messages/' + args);
+        };
+
+        ajaxFunctions.getMessages = function () {
+            return $http.get(urlBase + '/messages');
+        };
+
+        ajaxFunctions.getMessage = function (args) {
+            return $http.get(urlBase + '/messages/' + args);
+        };
+
+        ajaxFunctions.modifyMessage = function (args) {
+            return $http.put(urlBase + '/messages', args);
+        };
+
+        ajaxFunctions.postMessage = function (args) {
+            return $http.post(urlBase + '/messages', args);
+        };
+
+        ajaxFunctions.getThreads = function () {
+            return $http.get(urlBase + '/threads');
+        };
+
+        ajaxFunctions.getParentMessage = function () {
+            return $http.get(urlBase + '/parents');
+        };
+
+        ajaxFunctions.getThread = function (args) {
+            return $http.get(urlBase + '/threads/' + args);
+        };
+
+        ajaxFunctions.reply = function (args) {
+            return $http.post(urlBase + '/threads', args);
+        };
+
+        ajaxFunctions.newUser = function (args) {
+            return $http.post(urlBase + '/users', args);
+        };
+
+        ajaxFunctions.deleteUser = function (args) {
+            return $http.delete(urlBase + '/users/' + args);
+        };
+
+        ajaxFunctions.getUsers = function () {
+            return $http.get(urlBase + '/users');
+        };
+
+        ajaxFunctions.getUser = function (args) {
+            return $http.get(urlBase + '/users/' + args);
+        };
+    
+        ajaxFunctions.saveImage = function (args, options) {
+            return $http.post(urlBase + '/images', args, options);
         };
 
         return ajaxFunctions;
-  });
+    });
